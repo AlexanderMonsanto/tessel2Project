4624113316302092e37532bb79027aa03ce4291b
alexandermonsanto <alexandermonsanto@gmail.com>
2015-09-19 12:58:45 -0700

    tessel project initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// Import the interface to Tessel hardware
+var tessel = require('tessel');
+
+// Set the led pins as outputs
+var led1 = tessel.led[2];
+var led2 = tessel.led[3];
+
+// Set initial LED states
+led1.output(1);
+led2.output(1);
+
+setInterval(function () {
+    console.log("I'm blinking! (Press CTRL + C to stop)");
+    // Toggle the led states
+    led1.toggle();
+    led2.toggle();
+}, 100);
